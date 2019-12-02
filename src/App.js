aad4ad08a9214ecd3d1e4bccdba9476de1fa7c0c
Jenny Bacon <jbacon@gocardless.com>
2019-12-02 11:25:33 +0000

    added test user to firebase db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SignUp from 'SignUp';
+import SignUp from './SignUp';
 
 function App() {
   return (
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import * as firebase from 'firebase';
 
 class SignUp extends React.Component {
+
+    constructor() {
+        super()
+        var firebaseConfig = {
+            apiKey: "AIzaSyCsBt8FtmgRwYPC-aXsvuZoLBRGu8bN8nk",
+            authDomain: "gc-ping-pong.firebaseapp.com",
+            databaseURL: "https://gc-ping-pong.firebaseio.com",
+            projectId: "gc-ping-pong",
+            storageBucket: "gc-ping-pong.appspot.com",
+            messagingSenderId: "301659271768",
+            appId: "1:301659271768:web:2888ac87cbf0cb8809102d"
+          };
+          // Initialize Firebase
+        var app = firebase.initializeApp(firebaseConfig);
+        this.database = firebase.database();
+        this.addUser = this.addUser.bind(this);
+
+    }
+
+    addUser() {
+        console.log('hi')
+        this.database.ref('users/1234').set({
+            username: 'test',
+        });
+    }
+
+
     render() {
         return (
-            <button className="SignUp">
+            <button className="SignUp" onClick={this.addUser}>
               Sign Up
             </button>
         );
