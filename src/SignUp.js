e556d64a205302a33e06e49a4a33c4dab3281e0a
Jenny Bacon <jbacon@gocardless.com>
2019-12-02 10:40:06 +0000

    first try

---
+++ src/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+    render() {
+        return (
+            <button className="SignUp">
+              Sign Up
+            </button>
+        );
+    }
+  }
+
+  
+export default SignUp;
+  
